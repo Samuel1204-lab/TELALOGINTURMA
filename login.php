3a2c60eb231bf361e83a595752ffe3abac9458ea
Samuel1204-lab <samukarodrigues07@gmail.com>
2024-11-01 17:35:17 -0400

    task001

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela Login</title>
+</head>
+<body>
+    <h2>TELA LOGIN</h2><br><br>
+    <form action="" method="post">
+        <label >Usuário:</label>
+        <input type="email" name = "nome" id = "" placeholder = "Digite seu email">
+    </form><br><br>
+    <label>Senha:</label><br><br>
+    <input type="password" name = "senha" id = "" placeholder = "**************"><br><br>
+    <input type="submit" value = "LOGAR">
+    <a href="cadastro.php">INSCREVA-SE</a>
+    
+</body>
+</html>
---
+++ usuario.php
@@ -0,0 +1,50 @@
+<?php 
+    Class Usuario
+    {
+        private $pdo;
+
+
+        public $msgerro = "";
+
+
+        public function conectar($nome,$host,$usuario, $senha)          
+        {
+           global $pdo;
+
+           try
+           {
+            $pdo = new PDO("mysql:dbname="  .$nome, $usuario, $senha);
+           }
+           catch(PDOException $erro)
+           {
+                $msgerro = $erro->getMessage();
+           }
+        }
+
+        public function cadastrar($nome,$telefone, $email,$senha){
+            global $pdo;
+            //verificar se o email já está sendo cadastrado
+            $sql = $pdo->prepare("SELECT id_usuario FROM usuario WHERE email = :maria"); // :maria significa que colocamos um apelido na variável email do PHP
+            $sql->bindValue(":maria",$email);
+            $sql->execute();
+
+            //verificar se existe email cadastrado
+            if($sql->rowCount() > 0){
+                return false;
+            }
+            else{
+                //cadastrar usuario
+                $sql = $pdo->prepare("INSERT INTO usuario(nome,telefone,email,senha) VALUES (:n,:t,:e,:s)");
+                $sql->bindValue(":n",$nome);
+                $sql->bindValue(":t",$telefone);
+                $sql->bindValue(":e",$email);
+                $sql->bindValue(":s",$senha);
+                $sql->execute();
+                return true;    
+            }
+        }
+    
+    
+    };
+
+?>
